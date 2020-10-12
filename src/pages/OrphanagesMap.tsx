73deb9c5336c3b35b1aaad8b95eece5956708e42
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-12 18:50:17 -0300

    criando pag do mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiPlus } from 'react-icons/fi'
+
+import mapMarkerImg from '../images/mapMark.svg'
+
+import '../styles/pages/orphanages-map.css'
 
 function OrphanagesMap() {
     return (
-        <h1>Hello world</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy" />
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>
+                        <strong>Brasília</strong>
+                        <span>Distrito Federal</span>
+                    </strong>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="fff" />
+            </Link>
+        </div>
     )
 }
 
