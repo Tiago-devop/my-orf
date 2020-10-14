37552df67676cdf55dd5597863d7a1ebf00854c1
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-14 18:52:25 -0300

    listando orfanato no mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiPlus, FiArrowRight } from 'react-icons/fi'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
@@ -7,9 +7,25 @@ import mapMarkerImg from '../images/map-marker.svg'
 import mapIcon from '../utils/mapIcon'
 
 import '../styles/pages/orphanages-map.css'
+import api from '../services/api'
+
+interface Orphanage {
+    id: number
+    latitude: number
+    longitude: number
+    name: string
+}
 
 
 function OrphanagesMap() {
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([])
+
+    useEffect(() => {
+        api.get('orphanages').then(response => {
+            setOrphanages(response.data)
+        })
+    }, [])
+
     return (
         <div id="page-map">
             <aside>
@@ -34,19 +50,25 @@ function OrphanagesMap() {
                 style={{ width: '100%', height: '100%' }}
             >
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-                <Marker
+            
+            {orphanages.map(orphanage => {
+                return (
+                    <Marker
                     icon={mapIcon}
-                    position={[-15.7939539, -47.8923366]}
+                    position={[orphanage.latitude, orphanage.longitude]}
+                    key={orphanage.id}
                 >
                     <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-                        Lar das meninas
-                        <Link to="/orphanages/1">
+                        {orphanage.name}
+                        <Link to={`/orphanages/${orphanage.id}`}>
                             <FiArrowRight size={20} color="#FFF" />
                         </Link>
                     </Popup>
 
-                </Marker>    
+                </Marker>
+                )
+            })}
+
             </Map>
 
 
