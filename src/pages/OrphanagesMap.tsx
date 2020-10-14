484c66bd0025850b189137afa50dbf3709f5ca85
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-14 15:49:41 -0300

    finalizando mapa cp pag. cri. nav entre telas

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FiPlus } from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet'
+import { FiPlus, FiArrowRight } from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import Leaflet from 'leaflet'
 
 import 'leaflet/dist/leaflet.css'
 
-import mapMarkerImg from '../images/mapMark.svg'
+import mapMarkerImg from '../images/map-marker.svg'
 
 import '../styles/pages/orphanages-map.css'
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
 
 function OrphanagesMap() {
     return (
@@ -29,15 +38,28 @@ function OrphanagesMap() {
             </aside>
 
             <Map
-                center={[-15.7939539,-47.8923366]}
+                center={[-15.7939539, -47.8923366]}
                 zoom={15}
                 style={{ width: '100%', height: '100%' }}
             >
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            </Map>    
+
+                <Marker
+                    icon={mapIcon}
+                    position={[-15.7939539, -47.8923366]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar das meninas
+                        <Link to="/orphanages/1">
+                            <FiArrowRight size={20} color="#FFF" />
+                        </Link>
+                    </Popup>
+
+                </Marker>    
+            </Map>
 
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="fff" />
             </Link>
         </div>
